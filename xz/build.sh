2959e4b9db18c567408071700f37bbf9ba0c1e98
Chawye Hsu <su+git@chawyehsu.com>
2024-11-05 00:10:52 +0800

    fix: update xz build script
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- xz/build.sh
+++ xz/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -ex
 
-cmake $CMAKE_ARGS ^
-    -G Ninja ^
-    -B build ^
-    -D BUILD_SHARED_LIBS=YES ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX ^
-    -D CMAKE_PREFIX_PATH=$LIBRARY_PREFIX ^
-    -S $SRC_DIR
+cmake ${CMAKE_ARGS} \
+    -G Ninja \
+    -B build \
+    -D BUILD_SHARED_LIBS=YES \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+    -D CMAKE_PREFIX_PATH=${PREFIX} \
+    -D BUILD_TESTING=NO \
+    -S ${SRC_DIR}
 
-cmake --build build -j $CPU_COUNT --target install
+cmake --build build -j ${CPU_COUNT} --target install
