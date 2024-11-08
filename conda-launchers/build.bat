887df9e1d1c947fe4b16b424a8899dde18eeb988
Chawye Hsu <su+git@chawyehsu.com>
2024-11-08 10:45:09 +0800

    refactor(conda-launchers): polish test script, move patches to src folder
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- conda-launchers/build.bat
+++ conda-launchers/build.bat
@@ -1,29 +1,30 @@
 setlocal EnableDelayedExpansion
 
-copy launcher.c.orig launcher.c /Y
+@rem rename patched source file to be used for building
+move /Y launcher.c.orig launcher.c
 
-set TARGET=x86_64-windows-gnu
+set ZIG_TARGET=x86_64-windows-gnu
 set EXE_TARGET=64
 if %TARGET_PLATFORM% == win-32 (
-    set TARGET=x86-windows-gnu
+    set ZIG_TARGET=x86-windows-gnu
     set EXE_TARGET=32
 )
 if %TARGET_PLATFORM% == win-arm64 (
-    set TARGET=aarch64-windows-gnu
+    set ZIG_TARGET=aarch64-windows-gnu
     set EXE_TARGET=arm64
 )
 
 @rem build cli launcher
-zig build -Doptimize=ReleaseSmall -Dtarget=%TARGET% -Dgui=false --prefix-exe-dir %SCRIPTS%
+zig build -Doptimize=ReleaseSmall -Dtarget=%ZIG_TARGET% -Dgui=false --prefix-exe-dir %SCRIPTS%
 @rem build gui launcher
-zig build -Doptimize=ReleaseSmall -Dtarget=%TARGET% -Dgui=true --prefix-exe-dir %SCRIPTS%
+zig build -Doptimize=ReleaseSmall -Dtarget=%ZIG_TARGET% -Dgui=true --prefix-exe-dir %SCRIPTS%
 
-IF %ERRORLEVEL% NEQ 0 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
-@REM @rem install launcher scripts
+@rem install launcher scripts
 cd %SCRIPTS%
 for %%f in (cli gui) do (
     echo print^("%%f-%EXE_TARGET%.exe successfully launched the accompanying Python script"^)> "%%f-%EXE_TARGET%-script.py"
 )
 
-IF %ERRORLEVEL% NEQ 0 exit 1
+if %ERRORLEVEL% neq 0 exit 1
--- conda-launchers/test.bat
+++ conda-launchers/test.bat
@@ -1,20 +1,15 @@
-@echo off
 setlocal EnableDelayedExpansion
 
-set TARGET=x86_64-windows-gnu
 set EXE_TARGET=64
 if %TARGET_PLATFORM% == win-32 (
-    set TARGET=x86-windows-gnu
     set EXE_TARGET=32
 )
 if %TARGET_PLATFORM% == win-arm64 (
-    set TARGET=aarch64-windows-gnu
     set EXE_TARGET=arm64
 )
 
 if not exist "%SCRIPTS%\cli-%EXE_TARGET%.exe" exit 1
 if not exist "%SCRIPTS%\cli-%EXE_TARGET%-script.py" exit 1
 CALL "%SCRIPTS%\cli-%EXE_TARGET%.exe"
-python "%SCRIPTS%\cli-%EXE_TARGET%-script.py"
 if not exist "%SCRIPTS%\gui-%EXE_TARGET%.exe" exit 1
 if not exist "%SCRIPTS%\gui-%EXE_TARGET%-script.py" exit 1
