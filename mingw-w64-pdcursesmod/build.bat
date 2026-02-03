3fbc40dd24377ea8bd02416e2a54508436010b1f
Chawye Hsu <su+git@chawyehsu.com>
2026-02-03 17:31:59 +0800

    mingw-w64-pdcursesmod: add recipe

---
+++ mingw-w64-pdcursesmod/build.bat
@@ -0,0 +1,27 @@
+cd wincon
+
+set "PDC_TARGET=_w64"
+if "%TARGET_PLATFORM%"=="win-arm64" (
+    set "PDC_TARGET=_a64"
+)
+
+make -f Makefile WIDE=Y DLL=Y UTF8=Y %PDC_TARGET%=Y
+if errorlevel 1 exit 1
+
+@rem install header files
+mkdir "%LIBRARY_PREFIX%\include\"
+for %%f in (
+    curses.h
+    panel.h
+    term.h
+) do copy "%SRC_DIR%\%%f" "%LIBRARY_PREFIX%\include\"
+
+@rem install dll
+mkdir "%LIBRARY_PREFIX%\bin\"
+copy pdcurses.dll "%LIBRARY_PREFIX%\bin\"
+
+@rem install lib
+mkdir "%LIBRARY_PREFIX%\lib\"
+copy pdcurses.a "%LIBRARY_PREFIX%\lib\pdcurses.a"
+@rem duplicate lib as curses.lib for compatibility
+copy pdcurses.a "%LIBRARY_PREFIX%\lib\curses.a"
