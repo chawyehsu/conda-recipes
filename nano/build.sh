d9f16327cf6af966276a67627798d3777952a2e7
Chawye Hsu <su+git@chawyehsu.com>
2026-02-04 12:34:34 +0800

    nano: support building for win
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- nano/build.sh
+++ nano/build.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-
 set -ex
 
-./configure --prefix="${PREFIX}"
+# shellcheck disable=SC2154
+if [[ "${target_platform}" =~ win-* ]]; then
+    # `PDC_FORCE_UTF8` is required to force PDC to be utf8 mode
+    export CFLAGS="$CFLAGS -DPDC_FORCE_UTF8"
+    export LDFLAGS="$LDFLAGS -L$LIBRARY_PREFIX/lib -static"
+    export NCURSESW_CFLAGS="-I$LIBRARY_PREFIX/include -DNCURSES_STATIC"
+    export NCURSESW_LIBS="-l:pdcurses.a"
 
-make
+    export HOST=x86_64-w64-mingw32
+
+    # there's a compiling failure relevant to the `browser`
+    # feature I couldn't workaround with, so disable it.
+    ./configure \
+        --prefix="${PREFIX}" \
+        --build="${BUILD}" \
+        --host="${HOST}" \
+        --enable-utf8 \
+        --disable-{nls,speller,browser}
+else
+    ./configure \
+        --prefix="${PREFIX}" \
+        --build="${BUILD}" \
+        --host="${HOST}"
+fi
+
+make -j "${CPU_COUNT}"
 make check
 make install
+
+# shellcheck disable=SC2154
+if [[ "${target_platform}" =~ win-* ]]; then
+  # remove the rnano symlink
+  rm "${PREFIX}/bin/rnano"
+fi
