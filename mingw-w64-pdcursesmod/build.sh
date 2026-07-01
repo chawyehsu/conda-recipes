1bc953958ad34e79767400f7a9ce5174f1e3502e
Chawye Hsu <su+git@chawyehsu.com>
2026-07-01 17:29:52 +0800

    mingw-w64-pdcursesmod: use conventional package structure
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- mingw-w64-pdcursesmod/build.sh
+++ mingw-w64-pdcursesmod/build.sh
@@ -3,24 +3,15 @@ set -ex
 
 pkg_name_static="mingw-w64-pdcursesmod-static"
 
-cd wincon
-
 PDC_TARGET=_w64
 # shellcheck disable=SC2154
 if [[ "${target_platform}" == "win-arm64" ]]; then
-    PDC_TARGET=_a64
+  PDC_TARGET=_a64
 elif [[ "${target_platform}" == "win-32" ]]; then
-    PDC_TARGET=_w32
-fi
-
-if [[ "${PKG_NAME}" == "$pkg_name_static" ]]; then
-    make -f Makefile WIDE=Y UTF8=Y ${PDC_TARGET}=Y
-else
-    make -f Makefile WIDE=Y UTF8=Y DLL=Y ${PDC_TARGET}=Y
+  PDC_TARGET=_w32
 fi
 
 # install header files
-cd ..
 mkdir -p "$LIBRARY_PREFIX/include/pdcurses"
 install -m 0644 curses.h panel.h term.h "$LIBRARY_PREFIX/include/pdcurses/"
 # needs defines matching the make step, see https://github.com/Bill-Gray/PDCursesMod/issues/133
@@ -31,14 +22,21 @@ EOF
 install -m 0644 pdcurses.h "$LIBRARY_PREFIX/include/pdcurses.h"
 # install `curses.h` as well for compatibility
 install -m 0644 pdcurses.h "$LIBRARY_PREFIX/include/curses.h"
+
 cd wincon
+if [[ "${PKG_NAME}" == "$pkg_name_static" ]]; then
+  make -f Makefile WIDE=Y UTF8=Y ${PDC_TARGET}=Y LIBNAME=pdcurses
+else
+  make -f Makefile WIDE=Y UTF8=Y DLL=Y ${PDC_TARGET}=Y LIBNAME=pdcurses
+fi
 
 if [[ ! "${PKG_NAME}" == "$pkg_name_static" ]]; then
-    # install dll
-    mkdir -p "$LIBRARY_PREFIX/bin"
-    install pdcurses.dll "$LIBRARY_PREFIX/bin/libpdcurses.dll"
+  # install dll
+  mkdir -p "$LIBRARY_PREFIX/bin"
+  install pdcurses.dll "$LIBRARY_PREFIX/bin/libpdcurses.dll"
 fi
 
 # install lib
 mkdir -p "$LIBRARY_PREFIX/lib"
 install pdcurses.a "$LIBRARY_PREFIX/lib/libpdcurses.a"
+cd ..
