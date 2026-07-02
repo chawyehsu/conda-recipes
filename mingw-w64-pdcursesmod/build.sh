4ecb7b7914c00bd5c824d8765165a6d8d97484be
Chawye Hsu <su+git@chawyehsu.com>
2026-07-02 22:45:28 +0800

    Refactor build.sh to improve file installation process
    Updated build script to use 'install' for copying files and added compatibility for 'curses.h'.

--- mingw-w64-pdcursesmod/build.sh
+++ mingw-w64-pdcursesmod/build.sh
@@ -20,25 +20,25 @@ else
 fi
 
 # install header files
+cd ..
 mkdir -p "$LIBRARY_PREFIX/include/pdcurses"
 install -m 0644 curses.h panel.h term.h "$LIBRARY_PREFIX/include/pdcurses/"
 # needs defines matching the make step, see https://github.com/Bill-Gray/PDCursesMod/issues/133
-# needs defines matching the make step, see https://github.com/Bill-Gray/PDCursesMod/issues/133
 cat > pdcurses.h <<'EOF'
-`#define` PDC_WIDE 1
-`#define` PDC_FORCE_UTF8 1
-`#include` "pdcurses/curses.h"
+#define PDC_FORCE_UTF8 1
+#include "pdcurses/curses.h"
 EOF
 install -m 0644 pdcurses.h "$LIBRARY_PREFIX/include/pdcurses.h"
+# install `curses.h` as well for compatibility
+install -m 0644 pdcurses.h "$LIBRARY_PREFIX/include/curses.h"
+cd wincon
 
 if [[ ! "${PKG_NAME}" == "$pkg_name_static" ]]; then
     # install dll
     mkdir -p "$LIBRARY_PREFIX/bin"
-    cp pdcurses.dll "$LIBRARY_PREFIX/bin/"
+    install pdcurses.dll "$LIBRARY_PREFIX/bin/libpdcurses.dll"
 fi
 
 # install lib
 mkdir -p "$LIBRARY_PREFIX/lib"
-cp pdcurses.a "$LIBRARY_PREFIX/lib/pdcurses.a"
-# duplicate lib as curses.a for compatibility
-cp "$LIBRARY_PREFIX/lib/pdcurses.a" "$LIBRARY_PREFIX/lib/curses.a"
+install pdcurses.a "$LIBRARY_PREFIX/lib/libpdcurses.a"
