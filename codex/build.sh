24158ebb3c9cfd9222d3e25ab58a39bdc999a546
Chawye Hsu <su+git@chawyehsu.com>
2026-08-24 16:47:05 +0800

    codex: init recipe
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

---
+++ codex/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# build
+just assemble-codex-package --cargo cargo-auditable --cargo-profile release --package-dir out --target "${CARGO_BUILD_TARGET}"
+
+# install artifacts
+cp -a out/bin/ "${PREFIX}/bin/"
+
+# Pixi: prevent CONDA_PREFIX from leaking into sandboxed processes
+mkdir -p "${PREFIX}/etc/pixi/codex"
+touch "${PREFIX}/etc/pixi/codex/global-ignore-conda-prefix"
